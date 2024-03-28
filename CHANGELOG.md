588daa2becddd684fdd6dfa68f393b890b7847aa
Crisenpuer <161527232+Crisenpuer@users.noreply.github.com>
2024-03-28 22:51:06 +0100

    Add files before sharing repository *facepalm*

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+# Version 1.0.1 [11-11-2023]
+
+### Features:
+  - Added polish locales
+
+
+# Version 1.0.0 [10-11-2023]
+
+### Changes:
+  - Added settings to change stack sizes (deafult: 1000)
+  - Added support for hazard concrete
+  - Mod no longer changes concrete recipes
+
+### Fixes:
+  - Because mod no loger changes recipes, concrete recipe is no longer broken (sorry for this terrible bug)
+
+
+# Version 0.1.1 [unknown]
+
+### Fixes:
+  - Added missing thumbnail.png file
+
+
+# Version 0.1.0  [date unknown]
+
+### Changes:
+  - Initial release.
---
+++ changelog-parser.py
@@ -0,0 +1,66 @@
+import os
+
+file = open('changelog.txt', 'r')
+changelog_in = []
+for line in file:
+    changelog_in.append(line)
+file.close()
+
+if not os.path.exists("changelog.txt"):
+    print("changelog.txt does not exist")
+else:
+    if os.path.exists("CHANGELOG.md"):
+        os.remove("CHANGELOG.md")
+    outfile = open("CHANGELOG.md", "x")
+
+    skip_next_line = False
+    changelog_out = []
+    versions_parsed = 0
+
+
+    for i in range(len(changelog_in)):
+        ##### Uncomment for debug process #####
+        # print(f'{i}/{len(changelog_in)-1}: {changelog_in[i]}')
+        entry:str = changelog_in[i].rstrip('\n')
+        if skip_next_line:
+            skip_next_line = False
+
+        else:
+            if len(changelog_out) == 0 and entry.startswith("Version:"):
+                formatted_line = entry.replace(':','')
+                print(f'Parsing {(formatted_line.lower()).replace('  ', ' ')}')
+                versions_parsed += 1
+                changelog_out.append(f'# {formatted_line} ')
+                if not 'Date:' in changelog_in[i+1]:
+                    changelog_out.append(f' [date unknown]\n')
+
+            elif entry.startswith("Version:"):
+                formatted_line = entry.replace(':','')
+                print(f'Parsing {formatted_line.lower()}')
+                versions_parsed += 1
+                changelog_out.append(f'\n\n# {formatted_line} ')
+                if not 'Date:' in changelog_in[i+1]:
+                    changelog_out.append(f' [date unknown]\n')
+
+            elif entry.startswith('Date:'):
+                formatted_line = entry.lstrip('Date: ')
+                formatted_line = formatted_line.replace('.','-')
+                if 'unknown' in formatted_line:
+                    formatted_line == 'date unknown'
+                changelog_out.append(f'[{formatted_line}]\n')
+
+            elif entry.startswith('  ') and entry.endswith(':'):
+                changelog_out.append(f'\n### {entry.lstrip('  ')}\n')
+
+            elif entry.startswith('    - '):
+                formatted_line = entry.replace('    ', '  ')
+                changelog_out.append(f'{formatted_line}\n')
+
+    for ent in changelog_out:
+        outfile.write(ent)
+
+    outfile.close()
+    if versions_parsed == 1:
+        print(f"\nParsed {len(changelog_in)-1} lines in 1 version")
+    else:
+        print(f"\nParsed {len(changelog_in)-1} lines in {versions_parsed} versions")
